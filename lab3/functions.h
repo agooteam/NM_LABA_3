9892aa188b530e651ff17cc097c26a6c3fc92ca3
Mikhail Nilov <mikhail.nilov.nstu@gmail.com>
2014-11-18 08:09:53 +0300

    Изменение файла functions.h
    Изменен тип TYPE с одинарной на двойную точность. В классы добавлена как
    закрытая так и открытая часть.

--- lab3/functions.h
+++ lab3/functions.h
@@ -4,17 +4,35 @@
 #include <iostream>
 
 using namespace std;
-typedef float TYPE;
-
-class matrix{
-	protected:
-
-	public:
-
-};
+typedef double TYPE;
 
 class vector{
 	protected:
-
+		int N;
+		TYPE *elem;
 	public:
+		TYPE square_norm();
+		TYPE square_norm_other(vector *v);
+		TYPE sum_vector(vector *v);
+		TYPE mul_scalar(TYPE *s);
+};
+
+class matrix{
+	protected:
+		int N;
+		int maxiter;
+		TYPE eps;
+		TYPE *ig;
+		TYPE *jg;
+		TYPE *ggl;
+		TYPE *ggu;
+		TYPE *di;
+		TYPE *pr;
+	public:
+		void read_kuslau();
+		void read_matrix_data(TYPE *mas);
+		TYPE mul_matrix_vector(vector *v);
+		void dec_LU_sq();
+		void allocation_memory();
+		void clear_memory();
 };
