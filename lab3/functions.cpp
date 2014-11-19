8b4683bda50b0cc892c82b07a53880e674ac7746
Mikhail Nilov <mikhail.nilov.nstu@gmail.com>
2014-11-19 08:20:44 +0300

    Изменение файла functions.h
    Удален класс вектор. В класс матрицы добавлены коэфиценты alfa,betta, а
    также добавлены вектора для работы с ЛОС. Изменены параметры функций
    чтения и умножения матрицы на вектор.

--- lab3/functions.cpp
+++ lab3/functions.cpp
@@ -1,1 +1,58 @@
 #include "functions.h"
+
+void matrix::read_kuslau(){
+	 ifstream input("kuslau.txt");
+	 input >> N;
+	 input >> maxiter;
+	 input >> eps;
+	 input.close();
+};
+
+void matrix::allocation_memory(){
+	ifstream input("ig.txt");
+	ig = new int[N+1];
+	for(int i = 0; i <= N; i++){ input >> ig[i] ; ig[i]--;}
+	int count_elem = ig[N] - ig[0];
+	jg = new int[count_elem];
+	ggl = new TYPE[count_elem];
+	ggu = new TYPE[count_elem];
+	pr = new TYPE[N];
+	di = new TYPE[N];
+	x = new TYPE[N];
+	r = new TYPE[N];
+	z = new TYPE[N];
+	p = new TYPE[N];
+};
+
+void matrix::clear_memory(){
+	delete [] ig;
+	delete [] jg;
+	delete [] ggu;
+	delete [] ggl;
+	delete [] pr;
+	delete [] di;
+	delete [] x;
+	delete [] r;
+	delete [] z;
+	delete [] p;
+};
+
+void matrix::read_matrix_data(){
+	ifstream input;
+	int count_elem = ig[N] - ig[0];
+	input.open("di.txt");
+	for(int i = 0; i < N ;i++) input >> di[i];
+	input.close();
+	input.open("pr.txt");
+	for(int i = 0; i < N ;i++) input >> pr[i];
+	input.close();
+	input.open("jg.txt");
+	for(int i = 0; i < count_elem; i++){ input >> jg[i] ; jg[i]--; }
+	input.close();
+	input.open("ggl.txt");
+	for(int i = 0; i < count_elem; i++) input >> ggl[i];
+	input.close();
+	input.open("ggu.txt");
+	for(int i = 0; i < count_elem; i++) input >> ggu[i];
+	input.close();
+};
--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,7 +1,13 @@
 #include "functions.h"
 
+matrix A;
+
+void solve(int solve_type){
+	
+};
 
 void main(){
-
+	A.read_kuslau();
+	A.allocation_memory();
 	system("pause");
 };
