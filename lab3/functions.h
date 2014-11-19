d6550f2debf0226f2e1d06767260e26d82d5ee2a
Mikhail Nilov <mikhail.nilov.nstu@gmail.com>
2014-11-19 08:20:54 +0300

    Revert "Изменение файла functions.h"
    This reverts commit 8b4683bda50b0cc892c82b07a53880e674ac7746.

--- lab3/functions.h
+++ lab3/functions.h
@@ -7,28 +7,32 @@
 using namespace std;
 typedef double TYPE;
 
+class vector{
+	protected:
+		int N;
+		TYPE *elem;
+	public:
+		TYPE square_norm();
+		TYPE square_norm_other(vector *v);
+		TYPE sum_vector(vector *v);
+		TYPE mul_scalar(TYPE *s);
+};
 
 class matrix{
 	protected:
 		int N;
 		int maxiter;
 		TYPE eps;
-		TYPE alfa;
-		TYPE betta;
 		int *ig;
 		int *jg;
 		TYPE *ggl;
 		TYPE *ggu;
 		TYPE *di;
 		TYPE *pr;
-		TYPE *x;
-		TYPE *r;
-		TYPE *z;
-		TYPE *p;
 	public:
 		void read_kuslau();
-		void read_matrix_data();
-		TYPE mul_matrix_vector(TYPE *v);
+		void read_matrix_data(TYPE *mas);
+		TYPE mul_matrix_vector(vector *v);
 		void dec_LU_sq();
 		void allocation_memory();
 		void clear_memory();
--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,13 +1,7 @@
 #include "functions.h"
 
-matrix A;
-
-void solve(int solve_type){
-	
-};
 
 void main(){
-	A.read_kuslau();
-	A.allocation_memory();
+
 	system("pause");
 };
